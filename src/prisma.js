f61ef035e08026f44cffc5a4a8c8ba25c5a0414b
AndreRdz7 <A01730716@itesm.mx>
2020-02-07 13:21:23 -0600

    query and mutation examples

--- src/prisma.js
+++ src/prisma.js
@@ -2,5 +2,33 @@ import { Prisma } from "prisma-binding";
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
-  endpoint: "localhost:4466"
+  endpoint: "http://localhost:4466"
 });
+
+// prisma.query.users(null, "{ id name email }").then(data => {
+//   console.log(JSON.stringify(data, undefined, 2));
+// });
+
+// prisma.mutation
+//   .createPost(
+//     {
+//       data: {
+//         title: "hello",
+//         body: "is it me you are looking for",
+//         published: true,
+//         author: {
+//           connect: {
+//             id: "ck6cfz83l003i0754dh9sgdnd"
+//           }
+//         }
+//       }
+//     },
+//     "{id title body published}"
+//   )
+//   .then(data => {
+//     console.log(JSON.stringify(data, undefined, 2 ));
+//     return prisma.query.users(null, "{id name posts {id title}}");
+//   })
+//   .then(data => {
+//     console.log(JSON.stringify(data, undefined, 2));
+//   });
